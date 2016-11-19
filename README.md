367722b310bf71052d38544cc410ac36e8d54728
anoopmundathan <anoop.mundathan@gmail.com>
2016-11-19 13:41:54 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# todoapp
+> Angular todo app
+
+## Getting Started
+### Setup
+```
+$ git clone
+$ cd todoapp
+```
+### Install
+```
+$ npm install
+```
+### Run
+```
+$ npm start
+```
---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+app.use('/', express.static('public'));
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('Server running at port ' + port);
+});
