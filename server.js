b38b9fa706a6c152641e225c1bbd3b37738b96f1
anoopmundathan <anoop.mundathan@gmail.com>
2016-11-19 14:34:41 +0000

    Install angular and Serve via node_module

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 'use strict';
 
 var express = require('express');
+var path = require('path');
 
 var app = express();
 
 app.use('/', express.static('public'));
+
+app.get('/vendor/angular.js', (req, res) => {
+	res.sendFile(path.join(__dirname, '/node_modules', 'angular', 'angular.min.js'));
+});
 
 var port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log('Server running at port ' + port);
 });
+
+console.log(path.join(__dirname));
